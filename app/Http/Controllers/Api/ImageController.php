d4a943647ab85f1c491f5aa4c71c78566d659969
AlexPutrya <alex.putrya@gmail.com>
2017-11-21 09:06:33 +0200

    Change local path to public

--- app/Http/Controllers/Api/ImageController.php
+++ app/Http/Controllers/Api/ImageController.php
@@ -14,49 +14,14 @@ use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
-    const IMAGE_CONFIG = [
-      'bulb_image' => [
-          'model' => 'App\Bulb',
-          'path' => '/img/bulbs/'],
-      'brand_image' => [
-          'model' => 'App\Brand',
-          'path' => '/img/brand/']
-    ];
 
+    public function save(Request $request){
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $filename = $image->getClientOriginalName();
+            $image->move(public_path() . $path, $filename);
 
-    public function getImageConfig($image_type) {
-
-        switch ($image_type) {
-            case 'brand':
-                return ['err' => false, 'config' => self::IMAGE_CONFIG['brand_image']];
-                break;
-            case 'bulb':
-                return ['err' => false, 'config' => self::IMAGE_CONFIG['bulb_image']];
-                break;
-            default:
-                return ['err' => true, 'message' => 'This image type invalid'];
-                break;
         }
-    }
-
-    public function save(Request $request) {
-
-            if ($request->input('type')) {
-                $image_config = $this->getImageConfig($request->input('type'));
-
-                if (!$image_config['err']) {
-
-                    extract($image_config['config']);
-                    if($request->hasFile('image')) {
-                        $image = $request->file('image');
-                        $filename = $image->getClientOriginalName();
-                        $image->move(public_path().$path, $filename);
-
-                    //return response()->json(['path' => $path, 'model' => $model], 200);
-                }
-                return response()->json(['err' => $image_config['err'], 'message' => $image_config['message']], 401);
-            }
-            return response()->json(['err' => true, 'message' => 'No type image value'], 200);
     }
 
     public function delete(Request $request) {
